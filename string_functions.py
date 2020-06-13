4bd6ad890d7960aa571d4edd48140a9b95a3b79c
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-13 16:08:57 -0500

    Add files via upload

--- string_functions.py
+++ string_functions.py
@@ -7,5 +7,9 @@ Function parameter assignment
 """
 
 
-def multiply_string(message,n):
+def multiply_string(message, n):
+    return message*n
 
+
+if __name__ == '__main__':
+    print(multiply_string("Spencer", 3))
--- test_string_functions.py
+++ test_string_functions.py
@@ -4,7 +4,7 @@ from Module6.more_functions.string_functions import multiply_string
 
 class MyTestCase(unittest.TestCase):
     def test_multiply_string(self):
-        self.assertEqual("Spencer", 3, "SpencerSpencerSpencer")
+        assert multiply_string("Spencer", 3) == "SpencerSpencerSpencer"
 
 
 if __name__ == '__main__':
