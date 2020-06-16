7f12f42aaccd76b80a9737b8179cdb2fef572072
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-16 14:20:46 -0500

    Add files via upload

--- Function_Default_Values.py
+++ Function_Default_Values.py
@@ -2,7 +2,6 @@
 Program: function_default_values.py
 Author: Spencer Cress
 Date: 6/13
-
 Function Default Values Assignment
 """
 
@@ -16,15 +15,29 @@ def score_input(test_name, test_score=0, invalid_message='Invalid test score, tr
     """
     #test_name = input("Input name of test: ")
     #test_score = input("input score test: ")
-    valid_input = test_name+": "+str(test_score)
-    if test_score < 0:
-        valid_input = invalid_message
-    if test_score > 100:
-        valid_input = invalid_message
+
+    """try:
+        test_score = int(test_score)
+    except:
+        print("Invalid Score")
+        raise ValueError"""
+    valid_input = 0
+    try:
+        test_score = float(test_score)
+    except ValueError:
+        valid_input = "this isn't a number, please enter one"
+    else:
+        valid_input = test_name+": "+str(test_score)
+        if test_score < 0:
+            valid_input = invalid_message
+        if test_score > 100:
+            valid_input = invalid_message
+        # else:
+            # valid_input = test_name+": "+str(test_score)
     return valid_input
 
 
 if __name__ == '__main__':
-    score_input("Math Test", 100)
+    score_input("Math Test", "machoman")
 
 
--- test_valid_input_in_functions.py
+++ test_valid_input_in_functions.py
@@ -4,10 +4,10 @@ from Module6.Topic4.Function_Default_Values import score_input
 
 class MyTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
-        self.assertEqual(score_input("Math Test"), "Math Test: 0")
+        self.assertEqual(score_input("Math Test"), "Math Test: 0.0")
 
     def test_score_input_test_score_valid(self):
-        self.assertEqual(score_input("Math Test", 100), "Math Test: 100")
+        self.assertEqual(score_input("Math Test", 100), "Math Test: 100.0")
 
     def test_score_input_test_score_below_range(self):
         self.assertEqual(score_input("Science Test", -10), "Invalid test score, try again!")
@@ -15,8 +15,9 @@ class MyTestCase(unittest.TestCase):
     def test_score_input_test_score_above_range(self):
         self.assertEqual(score_input("Physics Test", 212), "Invalid test score, try again!")
 
-    #def test_test_score_non_numeric(self):
-        #self.assertRaises(score_input("A true test of life..", "...You're still getting there, that's ok"), ValueError)
+    def test_test_score_non_numeric(self):
+        self.assertEqual(score_input("A true test of life..", "...You're still getting there, that's ok"), "this isn't a number, please enter one")
+        
 
 
 if __name__ == '__main__':
