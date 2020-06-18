a9e8eb769cf092e29a2e4c60fb6b1a15210f21d9
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-17 20:55:02 -0500

    Add files via upload

--- inner_functions_assignment.py
+++ inner_functions_assignment.py
@@ -8,7 +8,15 @@ Inner Functions assignment
 
 
 def measurements(a_list):
+    """
+    :param a_list: A list of dimensions for a rectangle that will be fed into underlying functions
+    :return: with help of other functions returns a message that contains the perimeter and area of said rectangle
+    """
     def area(b_list):
+        """
+        :param b_list: A list of dimensions for a rectangle that will be fed into function
+        :return: given the dimensions of a rectangle or square, returns area
+        """
         if len(a_list) == 1:
             area_x = a_list[0] ** 2
         if len(a_list) == 2:
@@ -16,6 +24,10 @@ def measurements(a_list):
         return area_x
 
     def perimeter(c_list):
+        """
+        :param c_list: A list of dimensions for a rectangle that will be fed into function
+        :return: given the dimensions of a rectangle or square, returns perimeter
+        """
         if len(a_list) == 1:
             perimeter_x = a_list[0] * 4
         if len(a_list) == 2:
